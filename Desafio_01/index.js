45b512fa0165ac730be75336ebd77e28d7e98dd4
Mateus CP <mateuscandicocp@gmail.com>
2023-03-14 10:09:35 -0300

    cursoJS-constructor

--- Desafio_01/index.js
+++ Desafio_01/index.js
@@ -2,26 +2,17 @@
 let bookAuthor = 'James Clear'
 let bookPages = 306
  */
-function createBook(title, author, pages) {
+function CreateBook(title, author, pages) {
     //object
-    const book = {
-        bookTitle: title,
-        bookAuthor: author,
-        bookPages: pages,
-        printBook: function () {
-            console.log('printing...')
-        }
-    }
-    return book
+    this.bookTitle = title;
+    this.bookAuthor = author;
+    this.bookPages = pages
 }
 
-const book1 = createBook('Atomic', 'James', 306)
-const book2 = createBook('Think', 'Napolean', 450)
-
-book1.color = 'White'
+const book1 = new CreateBook('Atomic', 'James', 306)
 
 console.log(book1)
-console.log(book2)
+
 //function
 
 /* function printBook() {
