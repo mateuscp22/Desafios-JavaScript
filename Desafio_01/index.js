bcadaba8d6d9da3bc9ec2751c1bd3a2663ba7313
Mateus CP <mateuscandicocp@gmail.com>
2023-03-14 10:25:28 -0300

    cursoJS-Objetos_Built_in

--- Desafio_01/index.js
+++ Desafio_01/index.js
@@ -1,22 +1,7 @@
-/* let bookTitle = 'Atomic Habits'
-let bookAuthor = 'James Clear'
-let bookPages = 306
- */
-function CreateBook(title, author, pages) {
-    //object
-    this.bookTitle = title;
-    this.bookAuthor = author;
-    this.bookPages = pages
-}
+// Maior numero dentro do array
+let array = (1, 2, 3, 4, 5, 6, 7, 8)
 
-const book1 = new CreateBook('Atomic', 'James', 306)
+console.log(Math.max(array))
 
-console.log(book1)
-
-//function
-
-/* function printBook() {
-    console.log('printing...')
-} */
-
-/* console.log(book) */
+/* Objetos Built in
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects */
