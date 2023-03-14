315cbb99891e3549655c0726e3fc627f9148b319
Mateus CP <mateuscandicocp@gmail.com>
2023-03-14 08:23:16 -0300

    cursoJS-quiz_dia_2

--- Desafio_01/index.js
+++ Desafio_01/index.js
@@ -1,2 +1,16 @@
-let favoriteFood = 'burguer'
-document.getElementById("text").innerHTML = favoriteFood 
+let bookTitle = 'Atomic Habits'
+let bookAuthor = 'James Clear'
+let bookPages = 306
+
+//object
+const book = {
+    bookTitle: 'Atomic Habits',
+    bookAuthor: 'James Clear',
+    bookPages: 306,
+    bookChapters: {
+        chap1: 'Fundamentals',
+        chap2: '1st law',
+    }
+}
+
+console.log(book)
